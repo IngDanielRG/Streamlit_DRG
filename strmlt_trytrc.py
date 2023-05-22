b69bed05c056d803f3a7a2edca61ecca0f696f9a
IngDanielRG <126293228+IngDanielRG@users.noreply.github.com>
2023-05-22 17:45:14 -0600

    Create strmlt_trytrc.py

---
+++ strmlt_trytrc.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def sidebar():
+    # st.title("O")
+    st.markdown("<h1 style='text-align: center; color: #FAB207;'> Rearreanged</h1>", unsafe_allow_html=True)
+    with st.sidebar:
+        st.header("Encabezado")
+        st.subheader("Subencabezado")
+        text = st.text_area("Filtrar ")
+
+        button0 = st.button("Golf")
+        if button0:
+            st.write(text)
+ 
+def header():
+  st.header("TRC")
+  col1, col2 = st.columns(2)
+  
+  with col1:
+    button1 = st.button("TRC?")
+        if button1:
+            st.write("Si")
+        elif not button1:
+            st.write("Golf")
+            
+   with col1:
+    button1 = st.button("TRC?")
+        if button1:
+            st.write("No")
+        elif not button1:
+            st.write("Golf'nt")
+            
+sidebar()
