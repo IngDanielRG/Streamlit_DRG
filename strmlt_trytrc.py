56dbeb5ed55796b80d1b132346f0ad60a2c85fed
IngDanielRG <126293228+IngDanielRG@users.noreply.github.com>
2023-05-22 18:38:23 -0600

    Update strmlt_trytrc.py

--- strmlt_trytrc.py
+++ strmlt_trytrc.py
@@ -8,14 +8,7 @@ game_data = pd.read_csv("Game.csv")
 def sidebar():
     # st.title("O")
     st.markdown("<h1 style='text-align: center; color: #7D3C98 ;'> Rearreanged</h1>", unsafe_allow_html=True)
-    with st.sidebar:
-        st.header("Encabezado")
-        st.subheader("Subencabezado")
-        text = st.text_area("Filtrar ")
-
-        button0 = st.button("Golf")
-        if button0:
-            st.write(text)
+   
  
 def header():
   st.header("TRC")
