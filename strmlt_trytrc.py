ed9629986279a05cc443134d2165a508912230e0
IngDanielRG <126293228+IngDanielRG@users.noreply.github.com>
2023-05-29 17:24:33 -0600

    Update strmlt_trytrc.py

--- strmlt_trytrc.py
+++ strmlt_trytrc.py
@@ -68,8 +68,7 @@ def chart():
         chart_data = pd.DataFrame(
             np.random.randn(20, 3),
             columns=['a', 'b', 'c'])
-
-            st.bar_chart(chart_data)
+        st.bar_chart(chart_data)
         
 sidebar()
 buttons()
