4f2a3d300c0e89eeb3a26e9d7e900325f4eb0c60
IngDanielRG <126293228+IngDanielRG@users.noreply.github.com>
2023-05-31 10:07:33 -0600

    Update strmlt_trytrc.py

--- strmlt_trytrc.py
+++ strmlt_trytrc.py
@@ -64,7 +64,7 @@ def chart():
         # Pie chart, where the slices will be ordered and plotted counter-clockwise:
         labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
         sizes = [15, 30, 45, 10]
-        # explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+        explode = (0, 0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
         fig1, ax1 = plt.subplots()
         ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
