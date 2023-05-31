a0e32fdc77b1058773d79b0cf17f3e4062beb82c
IngDanielRG <126293228+IngDanielRG@users.noreply.github.com>
2023-05-31 10:01:10 -0600

    Update strmlt_trytrc.py

--- strmlt_trytrc.py
+++ strmlt_trytrc.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+from streamlit_option_menu import option_men
+from matplotlib import pyplot as plt
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
